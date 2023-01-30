989f115acffce9fe1022328597dfafee2d7eaade
Christian Carollo <carollochristian6@gmail.com>
2023-01-30 18:00:41 +0100

    Modifica ad esercizio

--- src/main/java/co/develhope/mockdb/entities/Student.java
+++ src/main/java/co/develhope/mockdb/entities/Student.java
@@ -6,14 +6,14 @@ import javax.persistence.*;
 
 @Entity
 @Data
-@Table(name = "Student", uniqueConstraints = {
-        @UniqueConstraint(columnNames = { "email" })
-})
+@Table
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String firstName, lastName, email;
+    private String firstName, lastName;
+    @Column(unique = true, name = "emailUser")
+    private String email;
 
 
 
